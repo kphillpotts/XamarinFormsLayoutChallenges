f576bc941b642459f1b749aef18a5cf2839a3b4f
Kym Phillpotts <kphillpotts@gmail.com>
2017-03-08 15:39:06 +1100

    Basic ListView

---
+++ Timeline/Timeline/Timeline/DataFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Timeline.Models;
+
+namespace Timeline
+{
+    public static class DataFactory
+    {
+        public static IList<ExerciseClass> Classes { get; private set; }
+
+        static DataFactory()
+        {
+            Classes = new ObservableCollection<ExerciseClass>
+            {
+                new ExerciseClass
+                {
+                    ClassName = "Aerobics",
+                    Instructor = "Joe Smith",
+                    ClassTime = DateTime.Now.AddHours(2),
+                },
+                 new ExerciseClass
+                {
+                    ClassName = "Aerobics",
+                    Instructor = "Joe Smith",
+                    ClassTime = DateTime.Now.AddHours(-2),
+                },
+                 new ExerciseClass
+                {
+                    ClassName = "Aerobics",
+                    Instructor = "Joe Smith",
+                    ClassTime = DateTime.Now.AddHours(3),
+                },
+                 new ExerciseClass
+                {
+                    ClassName = "Aerobics",
+                    Instructor = "Joe Smith",
+                    ClassTime = DateTime.Now.AddHours(1),
+                },
+            };
+        }
+    }
+}
---
+++ Timeline/Timeline/Timeline/Models/ExerciseClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timeline.Models
+{
+    public class ExerciseClass
+    {
+        public DateTime ClassTime { get; set; }
+        public string ClassName { get; set; }
+        public string Instructor { get; set; }
+    }
+}
